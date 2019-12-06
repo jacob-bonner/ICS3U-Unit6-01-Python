cca53282b1f8a4c7c4b7a15b0dd0a92cd7a5f718
jacob-bonner <54957118+jacob-bonner@users.noreply.github.com>
2019-12-06 18:13:03 -0500

    Update numberlist.py

--- numberlist.py
+++ numberlist.py
@@ -11,17 +11,21 @@ import random
 def main():
     # This function adds 10 numbers to an array then calculates the average
 
-    # Array
+    # Variables and arrays
+    average = 0
     number_array = []
 
     # Process
     for counter in range(10):
         number = random.randint(1, 100)
         number_array.append(number)
+        print(number_array[counter])
+        average = average + number_array[counter]
 
-    average = sum(number_array)/10
+    average = average/10
 
     # Output
+    print("")
     print("The average of the random numbers is", average)
 
 
